b2191a57abd037d424a051908e440ec4d0b2516d
Roger Rudin <roger.rudin@gmail.com>
2019-07-19 22:32:48 +0200

    formatting

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,6 +1,6 @@
-import { EventEmitter } from "@angular/core";
-import { Observable } from "rxjs";
-import { scan, map, startWith, shareReplay, tap } from "rxjs/operators";
+import { EventEmitter } from '@angular/core';
+import { Observable } from 'rxjs';
+import { scan, map, startWith, shareReplay, tap } from 'rxjs/operators';
 
 // type Action<T> = (state: T) => T;
 interface Action<T> {
@@ -15,12 +15,12 @@ const actions$ = new EventEmitter<Action<any>>();
 
 data$ = actions$.pipe(
   scan((state: any, action: Action<any>) => {
-    console.log("before", state);
-    console.log("action", action);
-    console.log("evaluated action", action.eval(state));
+    console.log('before', state);
+    console.log('action', action);
+    console.log('evaluated action', action.eval(state));
     return { ...state, [action.name]: action.eval(state) };
   }, {}),
-  tap(state => console.log("after", state)),
+  tap(state => console.log('after', state)),
   shareReplay(1)
 );
 
@@ -28,15 +28,15 @@ data$.subscribe();
 
 export const useState = <T>(
   initial: T = null,
-  storeName: string = "global"
+  storeName: string = 'global'
 ) => {
   if (initial && storeName in nameMap) {
     throw new Error(`name ${storeName} already defined!!`);
   }
 
   nameMap.push(storeName);
-  console.log("storeName", storeName);
-  console.log("type", typeof initial);
+  console.log('storeName', storeName);
+  console.log('type', typeof initial);
   actions$.emit({
     eval: state => initial,
     name: storeName
@@ -47,7 +47,7 @@ export const useState = <T>(
   );
 };
 
-const setState = <T>(f: (s: T) => T, storeName: string = "global") => {
+const setState = <T>(f: (s: T) => T, storeName: string = 'global') => {
   actions$.emit({
     eval: s => f(s[storeName]),
     name: storeName
